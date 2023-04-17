a3dec75cda0b10d56aa6ab0f4a6b3ddc7ade6fde
markmind <markmind@users.noreply.github.com>
2023-04-17 07:36:39 +0000

    deploy: ac40fc5d3657019cbf7ae306d34024dd880be7b5

--- assets/operation.md.63d574b0.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,o as l,N as n}from"./chunks/framework.5bba019c.js";const q=JSON.parse('{"title":"Operation API","description":"","frontmatter":{},"headers":[],"relativePath":"operation.md","lastUpdated":1681204301000}'),o={name:"operation.md"},p=n("",42),e=[p];function t(c,r,F,y,D,i){return l(),a("div",null,e)}const d=s(o,[["render",t]]);export{q as __pageData,d as default};
--- assets/zh_strategy.md.913796b7.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as n,o as a,N as o}from"./chunks/framework.5bba019c.js";const u=JSON.parse('{"title":"策略API","description":"","frontmatter":{},"headers":[],"relativePath":"zh/strategy.md","lastUpdated":1681204301000}'),l={name:"zh/strategy.md"},p=o("",23),t=[p];function D(e,F,c,r,y,C){return a(),n("div",null,t)}const q=s(l,[["render",D]]);export{u as __pageData,q as default};
